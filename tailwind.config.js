cb5e6eac4349ccaf0af2ebdd5f87f321d4d442a9
motiongorilla <nize.afonya@gmail.com>
2024-12-17 01:36:19 +0000

    styled landing page. set main elements for future. added subrtle parallax to grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ module.exports = {
                 secondary: '#14a3ad',
                 secondarydark: "#1d484b",
                 accent: '#43e5b7',
-                danger: "#f44336",
+                dangermain: "#cd5e42",
                 textcolor: '#d1d1d1',
                 background: '#212121',
                 lightbg: "#292929",
                 infobg: "#21282b",
-                danger: "#cd5e42",
+                dangercolor: "#f44336",
                 warning: "#e3b466",
                 success: "#7e8e50",
                 important: "#cd4277",
@@ -23,15 +23,30 @@ module.exports = {
                 blacky: "#151515",
             },
             keyframes: {
-                'gradient-shift': {
+                "gradient-shift": {
                     '0%, 100%': { backgroundPosition: '0% 50%' },
                     '50%': { backgroundPosition: '100% 50%' },
+                },
+                caret_blink: {
+                    '0%, 100%': {
+                        opacity: '1',
+                    },
+                    '50%, 75%': {
+                        opacity: '0',
+                    }
+                },
+                typewriter: {
+                    to: {
+                        left: "100%"
+                    }
                 }
             },
             animation: {
-                'gradient': 'gradient-shift 15s ease infinite',
-            },
-        },
+                gradient: "gradient-shift 15s ease infinite",
+                typewriter: "typewriter 2s steps(11) forwards",
+                blink: "caret_blink 1.4s ease-in-out infinite",
+            }
+        }
     },
     plugins: [
         require("@tailwindcss/forms"),
